a39568f0a8cf1d86708b3e30406aaa6738350eaa
Akmal Sulaymonov <akmalsulaymonov@gmail.com>
2022-05-13 16:47:39 +0500

    Created other components

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { SVGProps } from 'react'
 
 interface Props {
     Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element,
@@ -9,7 +9,7 @@ function SidebarRow({Icon, title}: Props) {
   return (
     <div className="flex items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 cursor-pointer transition-all duration-200 group max-w-fit">
         <Icon className="h-6 w-6" />
-        <p className="group-hover:text-twitter">{title}</p>
+        <p className="hidden text-base font-light md:inline-flex lg:text-xl group-hover:text-twitter">{title}</p>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="mx-auto lg:max-w-6xl max-h-screen overflow-hidden">
       <Head>
         <title>Twitter Web App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="grid grid-cols-9">
 
         <Sidebar />
 
-        {/* Feed */}
+        <Feed />
 
-        {/* Widgets */}
+        <Widgets />
 
       </main>
 
