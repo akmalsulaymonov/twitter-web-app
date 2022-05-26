4181bb95068c86d36ec183756decb697bb62459b
Akmal Sulaymonov <akmalsulaymonov@gmail.com>
2022-05-26 13:43:07 +0500

    Add tweet functionality

--- pages/api/addTweet.ts
+++ pages/api/addTweet.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import TweetBody from '../../typings'
+import { TweetBody } from '../../typings'
 
 type Data = {
   message: string
@@ -28,7 +28,7 @@ export default async function handler(
         ]
     }
 
-    const apiEndpoint = `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}"`
+    const apiEndpoint = `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`
 
     const result = await fetch(apiEndpoint, {
         headers: {
